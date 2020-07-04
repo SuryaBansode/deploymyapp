c669a21f5ff0310fd798e7347977604b38d8fb27
R906566 <surya.bansode@cybg.com>
2020-07-04 09:15:04 +0100

    Updates

---
+++ precache-manifest.43b42a291f661e6daf5939f54dbae109.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae95b1a981c2cf15e220b72360098903",
+    "url": "/deploymyapp/index.html"
+  },
+  {
+    "revision": "1aa9a0c49d3a07a8fc0b",
+    "url": "/deploymyapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8b016cdaa2a980086d5d",
+    "url": "/deploymyapp/static/js/2.2164662f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploymyapp/static/js/2.2164662f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aa9a0c49d3a07a8fc0b",
+    "url": "/deploymyapp/static/js/main.27fbeb74.chunk.js"
+  },
+  {
+    "revision": "1346e425c8158f781f49",
+    "url": "/deploymyapp/static/js/runtime-main.d082f2cb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploymyapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
